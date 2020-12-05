71b9bc3cc77861153df3edeb4f57e3c0edb396a5
Rajax <67244169+Rajax-Plus@users.noreply.github.com>
2020-12-04 21:09:52 -0300

    Create action.js

---
+++ action.js
@@ -0,0 +1,9 @@
+function abrirjax21() {
+  document.getElementById("jax21").style.width = "100%";
+  document.getElementById("jax21").style.opacity = "1";
+}
+
+function fecharjax21() {
+  document.getElementById("jax21").style.width = "0";
+  document.getElementById("jax21").style.opacity = "0";
+}
