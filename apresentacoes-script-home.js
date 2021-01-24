8047bfb0942b21fccb14c7a13d789fec4a9de24c
Rajax <67244169+Rajax-Plus@users.noreply.github.com>
2021-01-23 21:23:15 -0300

    Create apresentacoes-script-home.js

---
+++ apresentacoes-script-home.js
@@ -0,0 +1,90 @@
+$(function (){
+    $('#titulo-rajax-apre-1-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-1-h').length) {
+                $('#iframeHolder-rajax-apre-1-h').html('<iframe id="iframe-rajax-apre-1-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-1.html"></iframe>');
+        }
+    });   
+});  
+
+
+$(function (){
+    $('#titulo-rajax-apre-2-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-2-h').length) {
+                $('#iframeHolder-rajax-apre-2-h').html('<iframe id="iframe-rajax-apre-2-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-2.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-rajax-apre-3-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-3-h').length) {
+                $('#iframeHolder-rajax-apre-3-h').html('<iframe id="iframe-rajax-apre-3-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-3.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-rajax-apre-4-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-4-h').length) {
+                $('#iframeHolder-rajax-apre-4-h').html('<iframe id="iframe-rajax-apre-4-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-4.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-rajax-apre-5-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-5-h').length) {
+                $('#iframeHolder-rajax-apre-5-h').html('<iframe id="iframe-rajax-apre-5-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-5.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-rajax-apre-6-h').click(function (){ 
+        if(!$('#iframe-rajax-apre-6-h').length) {
+                $('#iframeHolder-rajax-apre-6-h').html('<iframe id="iframe-rajax-apre-6-h" class="caixaflutuante-rajaxplus" src="https://www.rajax.com.br/p/apresentacao-6.html"></iframe>');
+        }
+    });   
+});  
+
+function abrirApre1h() {
+  if(document.getElementById('spoiler-rajax-apre-1-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-1-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-1-h') .style.display='none'}
+}
+function fecharApre1h() {
+  if(document.getElementById('spoiler-rajax-apre-1-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-1-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-1-h') .style.display='none'}
+}  
+
+function abrirApre2h() {
+  if(document.getElementById('spoiler-rajax-apre-2-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-2-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-2-h') .style.display='none'}
+}
+function fecharApre2h() {
+  if(document.getElementById('spoiler-rajax-apre-2-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-2-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-2-h') .style.display='none'}
+}  
+
+function abrirApre3h() {
+  if(document.getElementById('spoiler-rajax-apre-3-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-3-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-3-h') .style.display='none'}
+}
+function fecharApre3h() {
+  if(document.getElementById('spoiler-rajax-apre-3-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-3-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-3-h') .style.display='none'}
+}  
+
+function abrirApre4h() {
+  if(document.getElementById('spoiler-rajax-apre-4-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-4-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-4-h') .style.display='none'}
+}
+function fecharApre4h() {
+  if(document.getElementById('spoiler-rajax-apre-4-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-4-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-4-h') .style.display='none'}
+}  
+
+function abrirApre5h() {
+  if(document.getElementById('spoiler-rajax-apre-5-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-5-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-5-h') .style.display='none'}
+}
+function fecharApre5h() {
+  if(document.getElementById('spoiler-rajax-apre-5-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-5-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-5-h') .style.display='none'}
+}  
+
+function abrirApre6h() {
+  if(document.getElementById('spoiler-rajax-apre-6-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-6-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-6-h') .style.display='none'}
+}
+function fecharApre6h() {
+  if(document.getElementById('spoiler-rajax-apre-6-h') .style.display=='none') {document.getElementById('spoiler-rajax-apre-6-h') .style.display=''}else{document.getElementById('spoiler-rajax-apre-6-h') .style.display='none'}
+}  
