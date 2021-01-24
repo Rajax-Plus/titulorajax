01034693c8c6b74210b21b69ea0381f90468fd56
Rajax <67244169+Rajax-Plus@users.noreply.github.com>
2021-01-24 20:01:45 -0300

    Create canais-script-home.js

---
+++ canais-script-home.js
@@ -0,0 +1,89 @@
+$(function (){
+    $('#titulo-canal-1').click(function (){ 
+        if(!$('#iframe-canal-1').length) {
+                $('#iframeHolder-canal-1').html('<iframe id="iframe-canal-1" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/wnc.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-canal-2').click(function (){ 
+        if(!$('#iframe-canal-2').length) {
+                $('#iframeHolder-canal-2').html('<iframe id="iframe-canal-2" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/rajax.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-canal-3').click(function (){ 
+        if(!$('#iframe-canal-3').length) {
+                $('#iframeHolder-canal-3').html('<iframe id="iframe-canal-3" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/ranablewebs.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-canal-4').click(function (){ 
+        if(!$('#iframe-canal-4').length) {
+                $('#iframeHolder-canal-4').html('<iframe id="iframe-canal-4" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/ultra.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-canal-5').click(function (){ 
+        if(!$('#iframe-canal-5').length) {
+                $('#iframeHolder-canal-5').html('<iframe id="iframe-canal-5" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/webtv.html"></iframe>');
+        }
+    });   
+});  
+
+$(function (){
+    $('#titulo-canal-6').click(function (){ 
+        if(!$('#iframe-canal-6').length) {
+                $('#iframeHolder-canal-6').html('<iframe id="iframe-canal-6" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/canais/almanaquenovelas.html"></iframe>');
+        }
+    });   
+});  
+
+function abrirCanal1() {
+  if(document.getElementById('spoiler-canal-1') .style.display=='none') {document.getElementById('spoiler-canal-1') .style.display=''}else{document.getElementById('spoiler-canal-1') .style.display='none'}
+}
+function fecharCanal1() {
+  if(document.getElementById('spoiler-canal-1') .style.display=='none') {document.getElementById('spoiler-canal-1') .style.display=''}else{document.getElementById('spoiler-canal-1') .style.display='none'}
+}  
+
+function abrirCanal2() {
+  if(document.getElementById('spoiler-canal-2') .style.display=='none') {document.getElementById('spoiler-canal-2') .style.display=''}else{document.getElementById('spoiler-canal-2') .style.display='none'}
+}
+function fecharCanal2() {
+  if(document.getElementById('spoiler-canal-2') .style.display=='none') {document.getElementById('spoiler-canal-2') .style.display=''}else{document.getElementById('spoiler-canal-2') .style.display='none'}
+}
+
+function abrirCanal3() {
+  if(document.getElementById('spoiler-canal-3') .style.display=='none') {document.getElementById('spoiler-canal-3') .style.display=''}else{document.getElementById('spoiler-canal-3') .style.display='none'}
+}
+function fecharCanal3() {
+  if(document.getElementById('spoiler-canal-3') .style.display=='none') {document.getElementById('spoiler-canal-3') .style.display=''}else{document.getElementById('spoiler-canal-3') .style.display='none'}
+}
+
+function abrirCanal4() {
+  if(document.getElementById('spoiler-canal-4') .style.display=='none') {document.getElementById('spoiler-canal-4') .style.display=''}else{document.getElementById('spoiler-canal-4') .style.display='none'}
+}
+function fecharCanal4() {
+  if(document.getElementById('spoiler-canal-4') .style.display=='none') {document.getElementById('spoiler-canal-4') .style.display=''}else{document.getElementById('spoiler-canal-4') .style.display='none'}
+}
+
+function abrirCanal5() {
+  if(document.getElementById('spoiler-canal-5') .style.display=='none') {document.getElementById('spoiler-canal-5') .style.display=''}else{document.getElementById('spoiler-canal-5') .style.display='none'}
+}
+function fecharCanal5() {
+  if(document.getElementById('spoiler-canal-5') .style.display=='none') {document.getElementById('spoiler-canal-5') .style.display=''}else{document.getElementById('spoiler-canal-5') .style.display='none'}
+}
+
+function abrirCanal6() {
+  if(document.getElementById('spoiler-canal-6') .style.display=='none') {document.getElementById('spoiler-canal-6') .style.display=''}else{document.getElementById('spoiler-canal-6') .style.display='none'}
+}
+function fecharCanal6() {
+  if(document.getElementById('spoiler-canal-6') .style.display=='none') {document.getElementById('spoiler-canal-6') .style.display=''}else{document.getElementById('spoiler-canal-6') .style.display='none'}
+}
