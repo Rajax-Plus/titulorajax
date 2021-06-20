da0b30720d29c035298d0a32f31b3b30364351da
Rajax <67244169+Rajax-Plus@users.noreply.github.com>
2021-06-20 00:45:28 -0300

    Update and rename destaques-script-home-11.js to destaques-script-home-12.js

--- destaques-script-home-12.js
+++ destaques-script-home-12.js
@@ -9,7 +9,7 @@ $(function (){
 $(function (){
     $('#titulo-destaque-2').click(function (){ 
         if(!$('#iframe-destaque-2').length) {
-                $('#iframeHolder-destaque-2').html('<iframe id="iframe-destaque-2" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/atempestade.html"></iframe>');
+                $('#iframeHolder-destaque-2').html('<iframe id="iframe-destaque-2" class="caixaflutuante-rajaxplus" src="https://rajax-plus.github.io/garotasdesabado.html"></iframe>');
         }
     });   
 });  
